16b1aab3d94596ea12def1656e92265c48e353c1
duogwas <duong2001n@gmail.com>
2023-11-16 10:05:41 +0700

    StatelessWidget
    StatelessWidget, State là gì? Tự custom Widget extends StatelessWidget

--- widget_learning/widget_learning/lib/main.dart
+++ widget_learning/widget_learning/lib/main.dart
@@ -9,17 +9,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: SafeArea(child: Scaffold(
+    return MaterialApp(
+      home: SafeArea(
+          child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.pink,
-          title: const Text ('Widget Learning'),
+          title: const Text('Widget Learning'),
         ),
         body: const Center(
-          child: Text('Hello world'),
+          // child: Text('Hello world'),
+          child: MyWidget(true),
         ),
       )),
       debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+//state: trang thai cua input dau vao
+class MyWidget extends StatelessWidget {
+  final bool loading;
+  const MyWidget(this.loading);
+
+  @override
+  Widget build(BuildContext context) {
+    if (loading) {
+      return const CircularProgressIndicator();
+    } else {
+      return const Text('State');
+    }
   }
 }
