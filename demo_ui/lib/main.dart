eee39993b3de2009e7ff43ab4e3b152648687b5d
duogwas <duong2001n@gmail.com>
2024-03-14 09:26:28 +0700

    login page ui

--- demo_ui/lib/main.dart
+++ demo_ui/lib/main.dart
@@ -1,60 +1,72 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        // title: 'Flutter Demo',
-        // theme: ThemeData(
-        //   primarySwatch: Colors.blue,
-        // ),
-        debugShowCheckedModeBanner: false,
-        home: LoginPage());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
   }
 }
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login Page'),
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                  icon: Icon(Icons.email),
+                  hintText: "example@gmail.com",
+                  labelText: "Email"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.lock),
+                hintText: "your password",
+                labelText: "Password",
+              ),
+              obscureText: true,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              child: (Text(
+                'LOGIN',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              )),
+            ),
+          ],
         ),
-        body: Container(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(
-                    decoration: const InputDecoration(
-                        icon: Icon(Icons.email),
-                        hintText: 'example@gmail.com',
-                        labelText: 'Email')),
-                const SizedBox(height: 20),
-                TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        icon: Icon(Icons.lock),
-                        hintText: 'Your password',
-                        labelText: 'Password')),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: (){},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    minimumSize: const Size(120, 50)
-                  ), 
-                  child: const Text('LOGIN', style: TextStyle(fontSize: 20),))
-              ],
-            )));
+      ),
+    );
   }
 }
